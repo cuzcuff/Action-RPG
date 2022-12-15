dcebf6f3addc8c810b86069a385da4d53bfa1ecb
cuzcuff <christianellingsen2@gmail.com>
2022-12-15 09:38:47 +0100

    Presentasjons commit

--- RPG/Player/Player.gd
+++ RPG/Player/Player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 
 const ACCELERATION = 500
-const MAX_SPEED = 100
+const MAX_SPEED = 100 
 const FRICTION = 500
 
-enum {
+enum { #liste over variabler
 	MOVE,
 	ROLL,
 	ATTACK
@@ -21,10 +21,10 @@ func _ready():
 	animationTree.active = true
 
 
-func _physics_process(delta):
+func _physics_process(delta): #innebyd funksjon som er relatert til fysikken i spillet
 	match state:
 		MOVE:
-			move_state(delta)
+			move_state(delta) #Når man beveger seg kjører
 		
 		ROLL:
 			pass
@@ -35,7 +35,7 @@ func _physics_process(delta):
 	
 
 		
-func move_state(delta):
+func move_state(delta): #funksjon for movement
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -44,10 +44,10 @@ func move_state(delta):
 	
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector) #Run - Vector2.ZERO = Velocity(variabel)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta) #matte for å regne ut velocity
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
